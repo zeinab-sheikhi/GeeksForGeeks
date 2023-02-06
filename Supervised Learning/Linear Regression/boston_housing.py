23648025345916e7b9711dc934830677ce20d471
zeinab shiekhi <zeinab.sheykhie@gmail.com>
2023-02-06 06:08:25 +0330

    downloaded another dataset with columns

--- Supervised Learning/Linear Regression/boston_housing.py
+++ Supervised Learning/Linear Regression/boston_housing.py
@@ -2,29 +2,33 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
-file_path = '/Users/macbookpro/Python/GeeksForGeeks/Machine Learning/Supervised Learning/Linear Regression/housing.csv'
+file_path = '/Users/macbookpro/Python/GeeksForGeeks/Machine Learning/Supervised Learning/Linear Regression/bostonhousing.csv'
 df = pd.read_csv(file_path)
-new_column_name = '0.00632, 18.00, 2.310, 0, 0.5380, 6.5750, 65.20, 4.0900, 1, 296.0, 15.30, 396.90, 4.98, 24.00'
-df.rename(columns={' 0.00632  18.00   2.310  0  0.5380  6.5750  65.20  4.0900   1  296.0  15.30 396.90   4.98  24.00': new_column_name}, inplace=True)
-# print(df.columns)
-df[['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PITRATIO', 'B', 'LSTAT', 'Price']] = df[new_column_name].str.split(pat=", ", expand=True)
-# df[['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PITRATIO', 'B', 'LSTAT', 'Price']] = df[new_column_name].str.split(", ", expand=True)
-# df[new_column_name] = df[new_column_name].str.split(", ", expand=True)
-# print(df.columns)
 
-# # print(df.columns.str.split(" ", expand=True))
-# df.drop(df.index[0])
-print(df.shape)
+X = df.iloc[:, :-1] 
+y = df.iloc[:, -1]
 
-# print(df.dtypes)
+xtrain, xtest, ytrain, ytest = train_test_split(X, y, test_size=0.2)
 
-# df.columns = [''] * len(df.columns)
+print("xtrain shape : ", xtrain.shape)
+print("xtest shape  : ", xtest.shape)
+print("ytrain shape : ", ytrain.shape)
+print("ytest shape  : ", ytest.shape)
 
-# columns = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PITRATIO', 'B', 'LSTAT', 'Price']
-# df.set_axis(columns, axis=1, inplace=False)
-# print((df.head()))
-# x = df.drop('24.00', axis=1)
-# print(x)
-# y = df.iloc[:]
-# xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size =0.2)
+regressor = LinearRegression()
+regressor.fit(xtrain, ytrain)
+y_pred = regressor.predict(xtest)
+
+plt.scatter(ytest, y_pred, color='b')
+plt.xlabel("Price: in $1000's")
+plt.ylabel("Predicted value")
+plt.title("True value vs predicted value : Linear Regression")
+plt.show()
+
+mse = mean_squared_error(ytest, y_pred)
+mae = mean_absolute_error(ytest, y_pred)
+print("Mean Square Error : ", mse)
+print("Mean Absolute Error : ", mae)
