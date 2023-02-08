2fadf49ac648ef64da701c3e9d8afeb968a23b44
zeinab shiekhi <zeinab.sheykhie@gmail.com>
2023-02-08 08:31:59 +0330

    dfferent stemmers in nLP

--- NLP/intro_to_nlp.py
+++ NLP/intro_to_nlp.py
@@ -15,6 +15,34 @@
 # Stemming is the process of getting the root form of a word. Stem or root is the part to which inflectional 
 # affixes (-ed, -ize, -de, -s, etc.) are added. 
 
+# Over-stemming: It refers to the situation where a stemmer produces a root form
+# that is not a valid word or is not the correct root form of a word. Over-stemming can also be regarded as false-positives.
+
+# Under-stemming: It refers to the situation where a stemmer does not produce the correct root form of a word
+# or does not reduce a word to its base form. It can be interpreted as false-negatives.
+# Approaches to this problem is to use techniques like semantic role labeling, sentiment analysis, context-based information, etc.
+# that help to understand the context of the text and make the stemming process more precise.
+# Stemmer algorithms: 
+
+# Porter's Stemmer: simple, speedy, only supports English
+
+# Lovin's Stemmer: fast, able to handle irregular plurals, but time consuming and fails to orm words from stem
+
+# Dawson Stemmer: fast, but complex
+
+# Krovetz Stemmer: light in nature, but inefficient
+
+# Xerox Stemmer 
+
+# N-Gram Stemmer: based on string comparisons and langugae dependant, but not time efficient
+
+# Snowball Stemmer: multi-lingual stemmer
+
+# Lancaster Stemmer: more aggressive and dynamic compared to the other two stemmers. 
+# The stemmer is really faster, but the algorithm is really confusing when dealing with small words.
+# But they are not as efficient as Snowball Stemmers.
+# The Lancaster stemmers save the rules externally and basically uses an iterative algorithm
+
 # Chunking is the process of extracting phrases from unstructured text and more structure to it. 
 # It is also known as shallow parsing. It is done on top of Part of Speech tagging. 
 # It groups word into “chunks”, mainly of noun phrases. Chunking is done using regular expressions.
