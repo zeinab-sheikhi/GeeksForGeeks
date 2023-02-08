1d6369378daea98c699a0b4b7e0bdc96502a0f8b
zeinab shiekhi <zeinab.sheykhie@gmail.com>
2023-02-08 09:25:42 +0330

    wordnet

---
+++ NLP/wordnet.py
@@ -0,0 +1,29 @@
+# WordNet is a large lexical database of English
+
+from nltk.corpus import wordnet 
+syns = wordnet.synsets(('program'))
+print(syns)
+
+# An example of a synset:
+print(syns[0].name())
+  
+# Just the word:
+print(syns[0].lemmas()[0].name())
+  
+# Definition of that first synset:
+print(syns[0].definition())
+  
+# Examples of the word in use in sentences:
+print(syns[0].examples())
+
+synonyms = []
+antonyms = []
+  
+for syn in wordnet.synsets("good"):
+    for lem in syn.lemmas():
+        synonyms.append(lem.name())
+        if lem.antonyms():
+            antonyms.append(lem.antonyms()[0].name())
+  
+print(set(synonyms))
+print(set(antonyms))
